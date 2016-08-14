e99f4f126b56feb232b4a9e62768818e2a147211
Kevin McKernan <kevin@mckernan.in>
2016-08-14 12:53:14 -0600

    working slack notification, update index page, add node-slack

--- server.js
+++ server.js
@@ -1,12 +1,49 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var Slack = require('node-slack');
+var slack = new Slack(process.env.SLACK_URL);
 var app     = express();
 var port    = process.env.PORT || 8080;
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
+	console.log(req.query);
 	res.render('index');
+});
+
+app.post('/email', function(req, res) {
+	var mail = req.body;
+	slack.send({
+		text: 'Email',
+		icon_emoji: ':envelope:',
+		username: mail.recipient,
+		attachments: [
+			{
+				fallback: 'Incoming email from ' + mail.sender + ' titled ' + mail.subject,
+				color: 'good',
+				fields: [
+					{
+						title: 'Sender',
+						value: mail.sender
+					},
+					{
+						title: 'Subject',
+						value: mail.subject
+					},
+				],
+			},
+			{
+				fallback: 'Email content',
+				text: mail['body-plain']
+			},
+		],
+
+	});
+	res.sendStatus(200);
 });
 
 app.listen(port, function() {
